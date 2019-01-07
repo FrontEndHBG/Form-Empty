4bdcaabdecc9242452bcaf9cf2c24e30134576a7
Max Carlquist <tenkaklet@gmail.com>
2019-01-07 14:02:20 +0100

    update to es6

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var form = document.getElementById('the-form-in-html');
+const form = document.getElementById('the-form-in-html');
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     var input = document.getElementById('input-in-form');
@@ -6,6 +7,18 @@ form.addEventListener('submit', function (e) {
     // conditional
     if(input.value === '') {
         return false;
+    } else {
+        alert(input.value);
+    }
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let input = document.querySelector('#input-in-form');
+
+    // Conditional
+    if (input.value === '') {
+        return false
     } else {
         alert(input.value);
     }
