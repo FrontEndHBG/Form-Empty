a94a9ded2443b26d2373f5e307a4e20402ce9258
Max Carlquist <tenkaklet@gmail.com>
2019-02-11 13:42:01 +0100

    removed es5

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
 const form = document.querySelector('#the-form-in-html');
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    var input = document.getElementById('input-in-form');
-    
-    // conditional
-    if(input.value === '') {
-        return false;
-    } else {
-        alert(input.value);
-    }
-});
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let input = document.querySelector('#input-in-form');
